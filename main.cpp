54b2dc90d4b594d330f0ebd842659d268dc4523f
TheFloatingBrain <1550298+TheFloatingBrain@users.noreply.github.com>
2026-02-12 23:56:46 -0500

    Added some code to test a push

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+uint64_t fib(uint32_t n)
+{
+	if(n <= 0)
+		return 0;
+	else if(n == 1 || n == 2)
+		return 1;
+	else
+		return fib(n - 2) + fib(n - 1);
+}
+
+int main(int argc, char** args)
+{
+	std::string line;
+	std::cout << "What is the Nth fibonacci number you wish to compute: ";
+	std::getline(std::cin, line);
+	std::cout << fib(std::stoi(line));
+}
+
